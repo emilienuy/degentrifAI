d80cc26723e8172eadf6291fb76cf05d37d89e25
panzerwaschbar <127913988+panzerwaschbar@users.noreply.github.com>
2024-06-10 17:35:03 -0400

    Header for T

---
+++ FrontEnd/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles.Header.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+const Header = ( ) => {
+    return (
+        <header>
+
+
+        </header>
+    );
+};
+
+export default Header;
+ 
