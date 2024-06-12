1d47497be4e55c3b120d519b898a4da12d286de2
panzerwaschbar <127913988+panzerwaschbar@users.noreply.github.com>
2024-06-12 16:02:49 -0400

    Header corrections

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -1,15 +1,39 @@
+// import React from 'react';
+// //import '../styles.Header.scss';
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+
+// const Header = ( ) => {
+//     return (
+//         <header>
+
+
+//         </header>
+//     );
+// };
+
+// export default Header;
+ 
+
 import React from 'react';
-//import '../styles.Header.scss';
+import '../styles/Header.scss';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
-const Header = ( ) => {
+import { Link } from 'react-router-dom';
+const Header = () => {
     return (
-        <header>
-
-
-        </header>
+        <div id="div_top_hypers">
+            <ul id="ul_tope_hypers">
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/demo">Demo</Link>
+                </li>
+                <li>
+                    <Link to="/team">Team</Link>
+                </li>
+            </ul>
+        </div>
     );
 };
-
 export default Header;
- 
+
