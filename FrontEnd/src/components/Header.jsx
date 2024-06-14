98617ea0b8e82dab34289487dea18a707db40b35
panzerwaschbar <127913988+panzerwaschbar@users.noreply.github.com>
2024-06-14 12:27:00 -0400

    Latest Header Layout

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -79,78 +79,86 @@
 //Newer header -- switched sides
 // 
 
- import React from 'react';
- import '../styles/Header.scss';
- import '@fortawesome/fontawesome-free/css/all.min.css';
- import { Link } from 'react-router-dom';
+//  import React from 'react';
+//  import '../styles/Header.scss';
+//  import '@fortawesome/fontawesome-free/css/all.min.css';
+//  import { Link } from 'react-router-dom';
 
- const Header = () => {
-     return (
-         <div id="div_top_hypers" className="header-container">
-             <div className="logo">
-                 <img src="path_to_your_logo_image" alt="Website Logo" />
-             </div>
-             <div className="nav-links">
-                 <ul id="ul_tope_hypers">
-                     <li>
-                         <Link to="/about">About</Link>
-                     </li>
-                     <li>
-                         <Link to="/demo">Demo</Link>
-                     </li>
-                     <li>
-                         <Link to="/team">Team</Link>
-                     </li>
-                     <li>
-                         <Link to="/process">Process</Link>
-                     </li>
-                 </ul>
-             </div>
-             <div className="lang-toggle-container">
-                 <button className="lang-toggle">                     <i className="fas fa-globe"></i> FR
-                 </button>
-             </div>
-         </div>
-     );
- };
+//  const Header = () => {
+//      return (
+//          <div id="div_top_hypers" className="header-container">
+//              <div className="logo">
+//                  <img src="path_to_your_logo_image" alt="Website Logo" />
+//              </div>
+//              <div className="nav-links">
+//                  <ul id="ul_tope_hypers">
+//                      <li>
+//                          <Link to="/about">About</Link>
+//                      </li>
+//                      <li>
+//                          <Link to="/demo">Demo</Link>
+//                      </li>
+//                      <li>
+//                          <Link to="/team">Team</Link>
+//                      </li>
+//                      <li>
+//                          <Link to="/process">Process</Link>
+//                      </li>
+//                  </ul>
+//              </div>
+//              <div className="lang-toggle-container">
+//                  <button className="lang-toggle">                     <i className="fas fa-globe"></i> FR
+//                  </button>
+//              </div>
+//          </div>
+//      );
+//  };
 
- export default Header;
+//  export default Header;
 
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import '../styles/Header.scss';
-// import '@fortawesome/fontawesome-free/css/all.min.css';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.scss';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-// const Header = () => {
-//     return (
-//         <header> 
+const Header = () => {
+    return (
+        <header className="header-container"> 
 
-//             {/* Header Toggle */}
-//             <section className="hd-toggle">
-//                 <div className="lang-toggle-container">
-//                     <button className="lang-toggle">
-//                         <i className="fas fa-globe"></i> FR
-//                     </button>
-//                 </div>
-//             </section>
+            {/* Header Toggle */}
+            <section className="hd-toggle">
+                <div className="search-container">
+                    <i className="fas fa-search search-icon"></i>
+                    <input type="text" placeholder="Search..." className="search-input" />
+                </div>
+                <div className="lang-toggle-container">
+                    <button className="lang-toggle">
+                        <i className="fas fa-globe"></i> FR
+                    </button>
+                </div>
+            </section>
 
-//             {/* Header Main = Logo & Navigation */}
-//             <section className="hd-main"> 
-//                 <div className="hd-logo">
-//                     <img src=" "alt="de.gentrificAition"/> {/* de.gentrificAItion logo SVG */}
-//                 </div>
+            {/* Header Main = Logo & Navigation */}
+            <section className="hd-main"> 
+                <div className="hd-logo">
+                    <img src="path_to_your_logo_image" alt="de.gentrificAItion"/> {/* Replace with actual logo path */}
+                </div>
 
-//                 <ul>
-//                     <li><Link to="/">About</Link></li>
-//                     <li><Link to = "/Process">Process</Link></li>
-//                     <li><Link to="/Demo">Demo</Link></li>
-//                     <li><Link to = "/Team">Team</Link></li> 
-//                 </ul>
-//             </section>
+                <nav className="nav-links">
+                    <ul>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/process">Process</Link></li>
+                        <li><Link to="/demo">Demo</Link></li>
+                        <li><Link to="/team">Team</Link></li> 
+                    </ul>
+                </nav>
+            </section>
 
-//         </header>
-//     );
-// };
+        </header>
+    );
+};
+
+export default Header;
 
 
 
