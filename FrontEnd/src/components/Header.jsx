5fb2f036ac5b115978d75361b329bac6b0ed8597
theresaquan0413 <theresaquan0413@gmail.com>
2024-06-19 17:00:34 -0400

    Update of Header

--- FrontEnd/src/components/Header.jsx
+++ FrontEnd/src/components/Header.jsx
@@ -7,23 +7,10 @@ import Logo from '../pictures/DegentrifAI.png';
 const Header = () => {
     return (
         <header className="header-container"> 
-            {/* Header Toggle */}
-            {/* <section className="hd-toggle">
-                <div className="search-container">
-                    <i className="fas fa-search search-icon"></i>
-                    <input type="text" placeholder="Search..." className="search-input" />
-                </div>
-                <div className="lang-toggle-container">
-                    <button className="lang-toggle">
-                        <i className="fas fa-globe"></i> FR
-                    </button>
-                </div>
-            </section> */}
-
-            {/* Header Main = Logo & Navigation */}
+            {/* Header Main - Logo & Navigation */}
             <section className="hd-main"> 
                 <div className="hd-logo">
-                    <img src={Logo} alt="de.gentrificAItion" className="logo" /> {/* Adjusted class name */}
+                    <img src={Logo} alt="de.gentrificAItion" className="logo" />
                 </div>
 
                 <nav className="nav-links">
