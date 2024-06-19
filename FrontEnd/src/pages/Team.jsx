53d8d5e792db0b5f473a0a8cd6b5c9992bdc0998
theresaquan0413 <theresaquan0413@gmail.com>
2024-06-19 15:32:37 -0400

    Teams Page Updated

--- FrontEnd/src/pages/Team.jsx
+++ FrontEnd/src/pages/Team.jsx
@@ -1,10 +1,109 @@
+// import React, { useState } from 'react';
+// import '../styles/Team.scss'; 
+
+// // Import images
+// import EmilyHong from '../pictures/Emily_Hong.JPG';
+// import TheresaQuan from '../pictures/Theresa_Quan.JPG';
+// import EmilieNuyttens from '../pictures/Emilie.JPG';
+// import Luth from '../pictures/luth.jpg'
+// import Ali from '../pictures/Ali.png'
+
+// const Team = () => {
+//   const [activeMember, setActiveMember] = useState(null);
+
+//   const toggleInfo = (index) => {
+//     setActiveMember(activeMember === index ? null : index);
+//   };
+
+//   const teamMembers = [
+//     {
+//       name: 'Emily Hong',
+//       imgSrc: EmilyHong,
+//       email: 'emily.hong@example.com',
+//       linkedIn: 'https://www.linkedin.com/in/emilyhong',
+//       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.',
+//     },
+//     {
+//       name: 'Yutong(Theresa) Quan',
+//       imgSrc: TheresaQuan,
+//       email: 'ytquan@umich.edu',
+//       linkedIn: 'www.linkedin.com/in/yutong-quan-41b0a9268',
+//       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.',
+//     },
+//     {
+//       name: 'Emilie Nuyttens',
+//       imgSrc: EmilieNuyttens,
+//       email: 'emilienuyttens@gmail.com',
+//       linkedIn: 'http://linkedin.com/in/emilienuyttens',
+//       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.',
+//     },
+//     {
+//       name: 'Luth Gallemaso',
+//       imgSrc: Luth, 
+//       email: 'luth.villafuerte@gmail.com',
+//       linkedIn: 'https://www.linkedin.com/in/luthgallemaso',
+//       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.',
+//     },
+//     {
+//       name: 'Nastaran Alizade',
+//       imgSrc: '', 
+//       email: 'nastaran.alizade@example.com',
+//       linkedIn: 'https://www.linkedin.com/in/nastaranalizade',
+//       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.',
+//     },
+//     {
+//       name: 'Ali Saheb Pasand',
+//       imgSrc: Ali, 
+//       email: 'nastaran.alizade@example.com',
+//       linkedIn: 'https://www.linkedin.com/in/nastaranalizade',
+//       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.',
+//     },
+//   ];
+
+//   return (
+//     <div className="leadership-team">
+//       <h1>Our Team</h1>
+//       <div className="team-members">
+//         {teamMembers.map((member, index) => (
+//           <div
+//             className={`team-member ${activeMember === index ? 'active' : ''}`}
+//             key={index}
+//             onClick={() => toggleInfo(index)}
+//           >
+//             {member.imgSrc ? (
+//               <img src={member.imgSrc} alt={member.name} className="member-photo" />
+//             ) : (
+//               <div className="member-photo-placeholder">
+//                 <h2>{member.name}</h2>
+//               </div>
+//             )}
+//             <div className="member-info">
+//               <h2>{member.name}</h2>
+//               <p>{member.description}</p>
+//               <p>Email: <a href={`mailto:${member.email}`}>{member.email}</a></p>
+//               <p>LinkedIn: <a href={member.linkedIn} target="_blank" rel="noopener noreferrer">{member.name}</a></p>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Team;
+
 import React, { useState } from 'react';
-import '../styles/Team.scss'; // 确保路径正确
+import '../styles/Team.scss'; 
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 // Import images
 import EmilyHong from '../pictures/Emily_Hong.JPG';
 import TheresaQuan from '../pictures/Theresa_Quan.JPG';
-import EmilieNuyttens from '../pictures/Emilie.jpg';
+import EmilieNuyttens from '../pictures/Emilie.JPG';
+import Luth from '../pictures/luth.jpg';
+import Ali from '../pictures/Ali.png';
 
 const Team = () => {
   const [activeMember, setActiveMember] = useState(null);
@@ -24,27 +123,34 @@ const Team = () => {
     {
       name: 'Yutong(Theresa) Quan',
       imgSrc: TheresaQuan,
-      email: 'yutong.quan@example.com',
-      linkedIn: 'https://www.linkedin.com/in/yutongquan',
+      email: 'ytquan@umich.edu',
+      linkedIn: 'https://www.linkedin.com/in/yutong-quan-41b0a9268',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.',
     },
     {
       name: 'Emilie Nuyttens',
       imgSrc: EmilieNuyttens,
-      email: 'emilie.nuyttens@example.com',
-      linkedIn: 'https://www.linkedin.com/in/emilienuyttens',
+      email: 'emilienuyttens@gmail.com',
+      linkedIn: 'http://linkedin.com/in/emilienuyttens',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.',
     },
     {
       name: 'Luth Gallemaso',
-      imgSrc: '', // Placeholder, no image
-      email: 'luth.gallemaso@example.com',
+      imgSrc: Luth,
+      email: 'luth.villafuerte@gmail.com',
       linkedIn: 'https://www.linkedin.com/in/luthgallemaso',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.',
     },
     {
       name: 'Nastaran Alizade',
-      imgSrc: '', // Placeholder, no image
+      imgSrc: '', 
+      email: 'nastaran.alizade@example.com',
+      linkedIn: 'https://www.linkedin.com/in/nastaranalizade',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.',
+    },
+    {
+      name: 'Ali Saheb Pasand',
+      imgSrc: Ali, 
       email: 'nastaran.alizade@example.com',
       linkedIn: 'https://www.linkedin.com/in/nastaranalizade',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.',
@@ -53,7 +159,7 @@ const Team = () => {
 
   return (
     <div className="leadership-team">
-      <h1>Our Leadership Team</h1>
+      <h1>Our Team</h1>
       <div className="team-members">
         {teamMembers.map((member, index) => (
           <div
@@ -72,7 +178,11 @@ const Team = () => {
               <h2>{member.name}</h2>
               <p>{member.description}</p>
               <p>Email: <a href={`mailto:${member.email}`}>{member.email}</a></p>
-              <p>LinkedIn: <a href={member.linkedIn} target="_blank" rel="noopener noreferrer">{member.name}</a></p>
+              <div className="ft-social">
+                <a href={member.linkedIn} target="_blank" rel="noopener noreferrer">
+                  <FontAwesomeIcon icon={faLinkedin} /> {/* LinkedIn icon */}
+                </a>
+              </div>
             </div>
           </div>
         ))}
@@ -82,3 +192,7 @@ const Team = () => {
 };
 
 export default Team;
+
+
+
+
