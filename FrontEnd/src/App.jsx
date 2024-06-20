3e61de164df332cab242239c4ad423efc45673fc
theresaquan0413 <theresaquan0413@gmail.com>
2024-06-20 15:36:25 -0400

    Update Cover Page and About Page

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,21 +1,19 @@
-// src/App.js
-
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Route, Routes } from 'react-router-dom';
+import CoverPage from './pages/Cover'
 import About from './pages/About';
 import Process from './pages/Process';
 import Demo from './pages/Demo';
 import Team from './pages/Team';
-
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<Demo />} />
+        <Route path='/' element={<CoverPage />} />
         <Route path="/about" element={<About />} />
         <Route path="/demo" element={<Demo />} />
         <Route path="/team" element={<Team />} />
--- FrontEnd/src/pages/About.jsx
+++ FrontEnd/src/pages/About.jsx
@@ -1,35 +1,48 @@
-// src/pages/About.jsx
-
-import React from 'react';
+import React, { useEffect } from 'react';
+import '../styles/About.scss'; 
 
 const About = () => {
-  return (
-    <div className="demo-container">
-    <h1>About Page</h1>
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus dolor vel euismod elementum. Aliquam magna ipsum, tristique sit amet sagittis eget, pharetra id erat. Fusce feugiat magna ante, in venenatis ex efficitur a. Suspendisse ipsum elit, dignissim non sem sit amet, aliquet auctor mi. Quisque ante augue, semper a lorem sed, viverra dictum urna. Cras at libero commodo, efficitur enim ac, ornare sapien. Nam a eros ac leo pharetra venenatis. Nullam hendrerit, enim a semper suscipit, mi ipsum convallis nibh, ut auctor purus sem vel felis. Fusce semper ultrices leo ut sollicitudin. Fusce venenatis pharetra metus et mollis.
+    useEffect(() => {
+        const element = document.querySelector('.about-container');
+        element.style.opacity = 0;
+        element.style.transform = 'translateY(20px)';
+        setTimeout(() => {
+            element.style.transition = 'opacity 0.5s ease, transform 0.5s ease';
+            element.style.opacity = 1;
+            element.style.transform = 'translateY(0)';
+        }, 100);
+    }, []);
 
-        Nullam eu dictum diam, at sollicitudin tellus. In leo ligula, commodo fermentum porttitor in, pretium porttitor dolor. Cras finibus, justo ut congue sagittis, lorem lorem tempor leo, in ultricies enim purus nec lectus. Nunc malesuada accumsan nibh at mattis. Etiam vitae aliquam dolor, at elementum ex. Donec id egestas elit. Integer eleifend mollis condimentum. Etiam luctus sapien quis purus feugiat, nec faucibus risus elementum. Curabitur ut erat ac sapien sodales vulputate non id nisi. Duis efficitur erat urna, nec placerat turpis tincidunt gravida. Sed non lectus vitae nisl accumsan hendrerit et sit amet dui. Nam ultrices velit ut mi tempus facilisis. Nullam sagittis tincidunt ipsum, id consequat risus consectetur non.
+    return (
+        <div className="about-container">
+            <div className="header-section">
+                <h1>About Page</h1>
+                <div className="video-container">
+                    <video width="100%" controls>
+                        <source src="path/to/video.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+            </div>
 
-        Pellentesque eu faucibus odio. Praesent ac pellentesque diam, non tempus lorem. Ut id nisl facilisis neque pellentesque commodo a a ex. Nulla nec hendrerit nibh. Nunc eu accumsan velit. Ut congue lectus at nisi blandit lacinia. Curabitur velit est, sollicitudin quis efficitur non, lobortis in felis. Ut ultricies ultrices efficitur. Integer lobortis, tortor eget iaculis mattis, mi elit bibendum arcu, eu malesuada lectus ipsum et sem. <br></br>
-        Suspendisse non sem lectus. Aliquam ac consequat lorem. Nunc justo felis, hendrerit et elit sed, suscipit congue sapien.
+            <section className="mission-section">
+                <h2>Our Mission</h2>
+                <p>
+                    Why degentrifAI? … <br />
+                    What is degentriA? … <br />
+                    How does it work? … <br />
+                    <a href="/process">Click here to learn more about our process.</a>
+                </p>
+            </section>
 
-        Suspendisse a nisi in augue tempus tristique et ultrices nunc. Proin a risus eget augue posuere ultricies. Morbi consectetur dolor sit amet nisl dapibus, at pellentesque turpis finibus. Morbi eu blandit ante. Cras aliquam tincidunt orci, non elementum diam rhoncus non. Nunc ornare ex sed nulla porta, sit amet rhoncus turpis vestibulum. Sed cursus semper sem, ut sodales metus tristique eget. Duis dolor enim, posuere vel erat quis, tempus maximus est.
-
-        Mauris eget dolor suscipit, tincidunt mauris a, placerat justo. In maximus fermentum posuere. Donec convallis justo ac ipsum vehicula, a rutrum ante ornare. Fusce fringilla metus nec lobortis feugiat. Donec convallis interdum vestibulum. Suspendisse consectetur mollis ex quis vulputate. Nullam mattis sapien a tristique pellentesque. Aliquam scelerisque et ex mollis rutrum. Sed semper mi non turpis posuere, nec finibus elit vehicula. Aenean congue sem nisi, eget aliquet lectus viverra eget. Maecenas semper metus ac vehicula fermentum. Aenean tortor erat, interdum vel augue quis, iaculis venenatis nunc. Aliquam elit sem, laoreet eu laoreet ut, posuere at sem. Pellentesque iaculis dui vel augue venenatis, at sollicitudin mi ornare. Mauris ultrices sit amet nisi non fringilla.<br></br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus dolor vel euismod elementum. Aliquam magna ipsum, tristique sit amet sagittis eget, pharetra id erat. Fusce feugiat magna ante, in venenatis ex efficitur a. Suspendisse ipsum elit, dignissim non sem sit amet, aliquet auctor mi. Quisque ante augue, semper a lorem sed, viverra dictum urna. Cras at libero commodo, efficitur enim ac, ornare sapien. Nam a eros ac leo pharetra venenatis. Nullam hendrerit, enim a semper suscipit, mi ipsum convallis nibh, ut auctor purus sem vel felis. Fusce semper ultrices leo ut sollicitudin. Fusce venenatis pharetra metus et mollis.
-
-        Nullam eu dictum diam, at sollicitudin tellus. In leo ligula, commodo fermentum porttitor in, pretium porttitor dolor. Cras finibus, justo ut congue sagittis, lorem lorem tempor leo, in ultricies enim purus nec lectus. Nunc malesuada accumsan nibh at mattis. Etiam vitae aliquam dolor, at elementum ex. Donec id egestas elit. Integer eleifend mollis condimentum. Etiam luctus sapien quis purus feugiat, nec faucibus risus elementum. Curabitur ut erat ac sapien sodales vulputate non id nisi. Duis efficitur erat urna, nec placerat turpis tincidunt gravida. Sed non lectus vitae nisl accumsan hendrerit et sit amet dui. Nam ultrices velit ut mi tempus facilisis. Nullam sagittis tincidunt ipsum, id consequat risus consectetur non.
-
-        Pellentesque eu faucibus odio. Praesent ac pellentesque diam, non tempus lorem. Ut id nisl facilisis neque pellentesque commodo a a ex. Nulla nec hendrerit nibh. Nunc eu accumsan velit. Ut congue lectus at nisi blandit lacinia. Curabitur velit est, sollicitudin quis efficitur non, lobortis in felis. Ut ultricies ultrices efficitur. Integer lobortis, tortor eget iaculis mattis, mi elit bibendum arcu, eu malesuada lectus ipsum et sem. <br></br>
-        Suspendisse non sem lectus. Aliquam ac consequat lorem. Nunc justo felis, hendrerit et elit sed, suscipit congue sapien.
-
-        Suspendisse a nisi in augue tempus tristique et ultrices nunc. Proin a risus eget augue posuere ultricies. Morbi consectetur dolor sit amet nisl dapibus, at pellentesque turpis finibus. Morbi eu blandit ante. Cras aliquam tincidunt orci, non elementum diam rhoncus non. Nunc ornare ex sed nulla porta, sit amet rhoncus turpis vestibulum. Sed cursus semper sem, ut sodales metus tristique eget. Duis dolor enim, posuere vel erat quis, tempus maximus est.
-
-        Mauris eget dolor suscipit, tincidunt mauris a, placerat justo.
-    </p>
-</div>
-  );
+            <section className="land-acknowledgment-section">
+                <h2>Land Acknowledgment</h2>
+                <p>
+                    As our project pertains to discussions of land and neighborhoods in Tiohtiá:ke/Montreal, we would like to acknowledge, recognize, and respect the Kanien’kehà:ka as the traditional custodians of the lands and waters which we stand on. Mila is situated on the traditional territory of the Kanien’kehà:ka, a place which has long served as a site of meeting and exchange amongst many First Nations including the Kanien’kehá:ka of the Haudenosaunee Confederacy, Huron/Wendat, Abenaki, and Anishinaabeg. In future iterations of degentrifAI, we hope to integrate more Indigenous expertise and perspectives into the design of our website and the mission of our project.
+                </p>
+            </section>
+        </div>
+    );
 };
 
 export default About;
-
