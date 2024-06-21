835e03e43bd3350fa5529a315481bc0d477b3803
theresaquan0413 <theresaquan0413@gmail.com>
2024-06-21 17:19:22 -0400

    Update Process Page

--- FrontEnd/src/pages/Process.jsx
+++ FrontEnd/src/pages/Process.jsx
@@ -1,31 +1,110 @@
-// ./pages/Process.jsx
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import '../styles/Process.scss';
+import { useInView } from 'react-intersection-observer';
+
+const Section = ({ title, children }) => {
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    threshold: 0.1,
+  });
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (inView) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  }, [inView]);
+
+  return (
+    <div ref={ref} className={`section ${isVisible ? 'animate' : ''}`}>
+      <h2>{title}</h2>
+      {children}
+    </div>
+  );
+};
 
 const Process = () => {
   return (
-    <div className="demo-container">
-      <h1>Process Page</h1>
+    <div className="process-container">
+      <Section title="LITERATURE REVIEW">
         <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus dolor vel euismod elementum. Aliquam magna ipsum, tristique sit amet sagittis eget, pharetra id erat. Fusce feugiat magna ante, in venenatis ex efficitur a. Suspendisse ipsum elit, dignissim non sem sit amet, aliquet auctor mi. Quisque ante augue, semper a lorem sed, viverra dictum urna. Cras at libero commodo, efficitur enim ac, ornare sapien. Nam a eros ac leo pharetra venenatis. Nullam hendrerit, enim a semper suscipit, mi ipsum convallis nibh, ut auctor purus sem vel felis. Fusce semper ultrices leo ut sollicitudin. Fusce venenatis pharetra metus et mollis.
-
-        Nullam eu dictum diam, at sollicitudin tellus. In leo ligula, commodo fermentum porttitor in, pretium porttitor dolor. Cras finibus, justo ut congue sagittis, lorem lorem tempor leo, in ultricies enim purus nec lectus. Nunc malesuada accumsan nibh at mattis. Etiam vitae aliquam dolor, at elementum ex. Donec id egestas elit. Integer eleifend mollis condimentum. Etiam luctus sapien quis purus feugiat, nec faucibus risus elementum. Curabitur ut erat ac sapien sodales vulputate non id nisi. Duis efficitur erat urna, nec placerat turpis tincidunt gravida. Sed non lectus vitae nisl accumsan hendrerit et sit amet dui. Nam ultrices velit ut mi tempus facilisis. Nullam sagittis tincidunt ipsum, id consequat risus consectetur non.
-
-        Pellentesque eu faucibus odio. Praesent ac pellentesque diam, non tempus lorem. Ut id nisl facilisis neque pellentesque commodo a a ex. Nulla nec hendrerit nibh. Nunc eu accumsan velit. Ut congue lectus at nisi blandit lacinia. Curabitur velit est, sollicitudin quis efficitur non, lobortis in felis. Ut ultricies ultrices efficitur. Integer lobortis, tortor eget iaculis mattis, mi elit bibendum arcu, eu malesuada lectus ipsum et sem. <br></br>
-        Suspendisse non sem lectus. Aliquam ac consequat lorem. Nunc justo felis, hendrerit et elit sed, suscipit congue sapien.
-
-        Suspendisse a nisi in augue tempus tristique et ultrices nunc. Proin a risus eget augue posuere ultricies. Morbi consectetur dolor sit amet nisl dapibus, at pellentesque turpis finibus. Morbi eu blandit ante. Cras aliquam tincidunt orci, non elementum diam rhoncus non. Nunc ornare ex sed nulla porta, sit amet rhoncus turpis vestibulum. Sed cursus semper sem, ut sodales metus tristique eget. Duis dolor enim, posuere vel erat quis, tempus maximus est.
-
-        Mauris eget dolor suscipit, tincidunt mauris a, placerat justo. In maximus fermentum posuere. Donec convallis justo ac ipsum vehicula, a rutrum ante ornare. Fusce fringilla metus nec lobortis feugiat. Donec convallis interdum vestibulum. Suspendisse consectetur mollis ex quis vulputate. Nullam mattis sapien a tristique pellentesque. Aliquam scelerisque et ex mollis rutrum. Sed semper mi non turpis posuere, nec finibus elit vehicula. Aenean congue sem nisi, eget aliquet lectus viverra eget. Maecenas semper metus ac vehicula fermentum. Aenean tortor erat, interdum vel augue quis, iaculis venenatis nunc. Aliquam elit sem, laoreet eu laoreet ut, posuere at sem. Pellentesque iaculis dui vel augue venenatis, at sollicitudin mi ornare. Mauris ultrices sit amet nisi non fringilla.<br></br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam maximus dolor vel euismod elementum. Aliquam magna ipsum, tristique sit amet sagittis eget, pharetra id erat. Fusce feugiat magna ante, in venenatis ex efficitur a. Suspendisse ipsum elit, dignissim non sem sit amet, aliquet auctor mi. Quisque ante augue, semper a lorem sed, viverra dictum urna. Cras at libero commodo, efficitur enim ac, ornare sapien. Nam a eros ac leo pharetra venenatis. Nullam hendrerit, enim a semper suscipit, mi ipsum convallis nibh, ut auctor purus sem vel felis. Fusce semper ultrices leo ut sollicitudin. Fusce venenatis pharetra metus et mollis.
-
-        Nullam eu dictum diam, at sollicitudin tellus. In leo ligula, commodo fermentum porttitor in, pretium porttitor dolor. Cras finibus, justo ut congue sagittis, lorem lorem tempor leo, in ultricies enim purus nec lectus. Nunc malesuada accumsan nibh at mattis. Etiam vitae aliquam dolor, at elementum ex. Donec id egestas elit. Integer eleifend mollis condimentum. Etiam luctus sapien quis purus feugiat, nec faucibus risus elementum. Curabitur ut erat ac sapien sodales vulputate non id nisi. Duis efficitur erat urna, nec placerat turpis tincidunt gravida. Sed non lectus vitae nisl accumsan hendrerit et sit amet dui. Nam ultrices velit ut mi tempus facilisis. Nullam sagittis tincidunt ipsum, id consequat risus consectetur non.
-
-        Pellentesque eu faucibus odio. Praesent ac pellentesque diam, non tempus lorem. Ut id nisl facilisis neque pellentesque commodo a a ex. Nulla nec hendrerit nibh. Nunc eu accumsan velit. Ut congue lectus at nisi blandit lacinia. Curabitur velit est, sollicitudin quis efficitur non, lobortis in felis. Ut ultricies ultrices efficitur. Integer lobortis, tortor eget iaculis mattis, mi elit bibendum arcu, eu malesuada lectus ipsum et sem. <br></br>
-        Suspendisse non sem lectus. Aliquam ac consequat lorem. Nunc justo felis, hendrerit et elit sed, suscipit congue sapien.
-
-        Suspendisse a nisi in augue tempus tristique et ultrices nunc. Proin a risus eget augue posuere ultricies. Morbi consectetur dolor sit amet nisl dapibus, at pellentesque turpis finibus. Morbi eu blandit ante. Cras aliquam tincidunt orci, non elementum diam rhoncus non. Nunc ornare ex sed nulla porta, sit amet rhoncus turpis vestibulum. Sed cursus semper sem, ut sodales metus tristique eget. Duis dolor enim, posuere vel erat quis, tempus maximus est.
-
-        Mauris eget dolor suscipit, tincidunt mauris a, placerat justo.
+          Provide an overview summary here...
+          In the initial phase of our project, we embarked on a comprehensive literature review encompassing studies on gentrification, urban renewal, and prior initiatives integrating machine learning with gentrification analysis. Our aim was to acquire a thorough understanding of the social, geographical, economic, and political dimensions of gentrification and its interface with artificial intelligence (AI).
+          ...
         </p>
+      </Section>
+
+      <Section title="EXPERT CONSULTATION">
+        <div className="expert-consultation-section">
+          <div className="expert-card">
+            <img src="path/to/image1.jpg" alt="GENUINE Team" />
+            <div className="expert-card-text">
+              <h3>GENUINE Team</h3>
+              <p>
+                Our decision-making process was further informed by insights from the GENUINE paper, which offers a map-based gentrification tool designed specifically for Canadian metropolitan areas (Kestens et al., 2021)...
+              </p>
+            </div>
+          </div>
+          <div className="expert-card">
+            <img src="path/to/image2.jpg" alt="Kevin Manaugh" />
+            <div className="expert-card-text">
+              <h3>Curbcut Team, Kevin Manaugh, PhD</h3>
+              <p>
+                Impressed by his website, Curbcut, we reached out to Professor Kevin Manaugh, a McGill Professor in Geography...
+              </p>
+            </div>
+          </div>
+          <div className="expert-card">
+            <img src="path/to/image3.jpg" alt="Marie Sterlin" />
+            <div className="expert-card-text">
+              <h3>Marie Sterlin</h3>
+              <p>
+                Marie Sterlin works for the city of Montreal as a Borough Councillor and a member of the Commission on Social Development and Montreal Diversity...
+              </p>
+            </div>
+          </div>
+        </div>
+      </Section>
+
+      <Section title="DATA COLLECTION AND CLEANING">
+        <p>Details about data collection and cleaning...</p>
+      </Section>
+
+      <Section title="MODEL TRAINING">
+        <div className="model-content">
+          <div className="model-text">
+            <p>Details about model training...</p>
+          </div>
+          <div className="model-images">
+            <img src="path/to/image3.jpg" alt="Model Training" />
+          </div>
+        </div>
+      </Section>
+
+      <Section title="FINAL PROTOTYPE">
+        <div className="prototype-content">
+          <div className="prototype-text">
+            <p>Details about the final prototype...</p>
+          </div>
+          <div className="prototype-images">
+            <img src="path/to/image4.jpg" alt="Final Prototype" />
+          </div>
+        </div>
+      </Section>
+
+      <Section title="FUTURE APPLICATIONS">
+        <p>Details about future applications...</p>
+      </Section>
+
+      <Section title="REFERENCES">
+        <ul>
+          <li>Alejandro, Y., & Palafox, L. (2019). Gentrification Prediction Using Machine Learning...</li>
+          <li>Bekkerman, R., Warde, D., & Cohen, M. C. (2023). Measuring Gentrification...</li>
+          
+        </ul>
+      </Section>
     </div>
   );
 };
